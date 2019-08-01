77cb7396dae97dd7af82a780501b5e8f7df1345e
Carsten Schnober <schnober@textkernel.nl>
2019-08-01 10:59:20 +0200

    Remove unnecessary name declaration for Boat class.

--- rentomatic/models/transport.py
+++ rentomatic/models/transport.py
@@ -53,7 +53,7 @@ class Bike(Transport):
 
 class Boat(Transport):
     def __init__(self, price: float = 100.0, seats: int = 8):
-        super().__init__(price, seats, "boat")
+        super().__init__(price, seats)
 
 
 class Car(Transport):
